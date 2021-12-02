a88e2cd3d69bbdbbf9ce455c6be21981f49077b6
Flo <fgesell@hotmail.de>
2021-12-02 08:57:25 +0100

    fixed vue build

---
+++ frontend/src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import store from "../store";
+
+import Home from "../views/Home.vue";
+import Style from "../views/Style.vue";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home,
+    },
+    {
+      path: "/styleguide",
+      name: "Style",
+      component: Style,
+    },
+    {
+      path: "*",
+      redirect: "/",
+    },
+  ],
+});
+
+export default router;
