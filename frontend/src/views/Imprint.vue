7c4cd0ab89eeb06551f297152f38f4fe069f2c13
Flo <fgesell@hotmail.de>
2021-12-11 11:11:26 +0100

    added Imprint

---
+++ frontend/src/views/Imprint.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <div class="imprint">
+  </div>
+  
+</template>
+
+<script>
+
+export default {
+  name: 'Imprint',
+  components: {
+  },
+  data() {
+    return {
+      publicPath: process.env.BASE_URL
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+</style>
