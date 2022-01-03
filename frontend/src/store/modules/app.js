2e1c29bb88eea495ee9c12d1a77777cb9ee71c8b
Flo <fgesell@hotmail.de>
2022-01-03 11:19:13 +0100

    added specific tab & added info text

--- frontend/src/store/modules/app.js
+++ frontend/src/store/modules/app.js
@@ -8,6 +8,7 @@ Vue.use(Vuex)
 
 const state = {
     activeTab: 'Per Country',
+    activeSpecific: '',
     tabs: ['Per Country', 'Per Sector', 'Per person'],
     info: false,
     loader: true,
--- frontend/src/views/Info.vue
+++ frontend/src/views/Info.vue
@@ -1,15 +1,25 @@
 
 <template>
   <div class="info">
+    <Logo id="logo" :checked=false :dark=false />
+    <div class="text">
+      <h2>Informations</h2>
+      <p>Later various elements of out application will be explained here and supported by the scientific sources.</p>
+      </div>
+    <Button :text='"Back"' id="probButton" @click="$router.push('/')" />
   </div>
   
 </template>
 
 <script>
+import Logo from '@/components/Logo.vue'
+import Button from '@/components/buttons/Button.vue'
 
 export default {
   name: 'Info',
   components: {
+    Button,
+    Logo
   },
   data() {
     return {
@@ -20,4 +30,28 @@ export default {
 
 
 <style lang="scss" scoped>
+  .info {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  #logo {
+    margin-bottom: 50px;
+    width: 100%;
+    max-width: 340px;
+  } 
+
+  .text {
+    margin: 50px 0;
+    text-align: center;
+    h2 {
+      margin-bottom: 25px;
+    }
+  }
+
+  button {
+    max-width: 130px;
+  }
 </style>
