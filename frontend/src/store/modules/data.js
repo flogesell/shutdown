bd9f430dcd69811b2153d0745e9a8e0d91bedd36
Karl Diedrichsen <kdiedrichsen@me.com>
2022-01-10 14:05:53 +0100

    updated CAIT data to match the sectors

--- frontend/src/store/modules/data.js
+++ frontend/src/store/modules/data.js
@@ -6,16 +6,16 @@ Vue.use(Vuex)
 
 const state = {
     CAIT: [
-        { name: "China",         iso: "CHN",   total_emissions: 11705.81, co2_emissions: [5214.20,  917.02,  197.57, 5377.02] },
-        { name: "United States", iso: "USA",   total_emissions:  5794.35, co2_emissions: [2103.17, 1762.23,  133.24, 1795.71] },
-        { name: "India",         iso: "IND",   total_emissions:  3346.63, co2_emissions: [1241.34,  305.33,   83.17, 1716.79] },
-        { name: "Russia",        iso: "RUS",   total_emissions:  1992.08, co2_emissions: [ 844.67,  258.86,  117.21,  771.34] },
-        { name: "Indonesia",     iso: "IDN",   total_emissions:  1703.86, co2_emissions: [ 243.35,  154.01,  133.84, 1172.66] },
-        { name: "Brazil",        iso: "BRA",   total_emissions:  1420.58, co2_emissions: [  88.44,  191.66,   70.22, 1070.26] },
-        { name: "Japan",         iso: "JPN",   total_emissions:  1154.72, co2_emissions: [ 561.86,  204.56,    6.80,  381.50] },
-        { name: "Iran",          iso: "IRN",   total_emissions:   828.34, co2_emissions: [ 201.29,  139.23,   46.65,  441.17] },
-        { name: "Germany",       iso: "GER",   total_emissions:   776.61, co2_emissions: [ 310.35,  158.30,    8.63,  299.33] },
-        { name: "Others",        iso: "OTHER", total_emissions: 20555.84, co2_emissions: [6577.74, 4827.08, 2826.97, 6324.05] }
+        { name: "China",         iso: "CHN",   total_emissions: 11705.81, co2_emissions: [5214.2,   917.02,  672.87, 4901.72] },
+        { name: "United States", iso: "USA",   total_emissions:  5794.35, co2_emissions: [2103.17, 1762.23,  385.25, 1543.70] },
+        { name: "India",         iso: "IND",   total_emissions:  3346.63, co2_emissions: [1241.34,  305.33,  718.70, 1081.26] },
+        { name: "Russia",        iso: "RUS",   total_emissions:  1992.08, co2_emissions: [ 844.67,  258.86,   95.99,  792.56] },
+        { name: "Indonesia",     iso: "IDN",   total_emissions:  1703.86, co2_emissions: [ 243.35,  154.01,  200.24, 1172.66] },
+        { name: "Brazil",        iso: "BRA",   total_emissions:  1420.58, co2_emissions: [  88.44,  191.66,  496.10,  644.38] },
+        { name: "Japan",         iso: "JPN",   total_emissions:  1154.72, co2_emissions: [ 561.86,  204.56,   21.56,  366.74] },
+        { name: "Iran",          iso: "IRN",   total_emissions:   828.34, co2_emissions: [ 201.29,  139.23,   32.74,  455.08] },
+        { name: "Germany",       iso: "GER",   total_emissions:   776.61, co2_emissions: [ 310.35,  158.30,   58.30,  249.66] },
+        { name: "Others",        iso: "OTHER", total_emissions: 20555.84, co2_emissions: [6577.74,4827.08,  5146.13, 4004.89] }
     ],
     population: [
         { name: "China",         population: 1433.784 },
