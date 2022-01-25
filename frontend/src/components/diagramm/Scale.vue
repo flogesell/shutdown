51daa86e029659433ac043e8da46017dc6e1ec49
Flo <fgesell@hotmail.de>
2022-01-25 13:55:57 +0100

    added CO₂

--- frontend/src/components/diagramm/Scale.vue
+++ frontend/src/components/diagramm/Scale.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="ring" v-for="(ring, index) in rings" :key="ring.x" :style="{'left': ring.x + 'px', 'top': ring.y + 'px', 'height': ring.diameter + 'px', 'width': ring.diameter + 'px', 'border-color': current_color, 'display': (!(index % 2)) ? '' : 'none'}">
             <div v-if="!(index % 2)" class="legend" :style="{'border-color': current_color}">
-                <div class="test" :style="{'color': current_color}">{{ (ring.co2 + ' Gt CO2') }} </div>
+                <div class="test" :style="{'color': current_color}">{{ (ring.co2 + ' Gt CO₂') }} </div>
             </div>
         </div>
     </div>
